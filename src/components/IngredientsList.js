686fb20ead1154ba847d4ed2e34a708034742c78
Zelia Mauro <zeliamauro@Zelias-MacBook-Air.local>
2023-07-27 12:16:19 -0500

    YAYYYY bug is fixed

--- src/components/IngredientsList.js
+++ src/components/IngredientsList.js
@@ -9,6 +9,7 @@ export default function IngredientsList() {
 
   const addItem = (ingredientText, ingredient, quantity, units) => {
     const new_id = uuid();
+
     const newIngredients = [
       {
         text: ingredientText,
@@ -20,6 +21,7 @@ export default function IngredientsList() {
       },
       ...ingredients,
     ];
+
     setIngredients(newIngredients);
   };
 
@@ -52,11 +54,12 @@ export default function IngredientsList() {
     );
   };
 
-  const handleHalve = () => {
+  const handleChange = (multiplier) => {
     const newIngredients = [];
+    console.log("hello");
 
     for (let i = 0; i < ingredients.length; i++) {
-      const newQuantStr = (ingredients[i].quant / 2).toFixed(1);
+      const newQuantStr = (ingredients[i].quant * multiplier).toFixed(1);
 
       console.log(newQuantStr);
 
@@ -65,45 +68,10 @@ export default function IngredientsList() {
       console.log(newQuant);
 
       const plural_adjusted =
-        newQuant === 1 ? ingredients[i].units : ingredients[i].units + "s";
+        newQuant === 1 ? ingredients[i].unit : ingredients[i].unit + "s";
 
       const units_adjusted =
-        ingredients[i].units === "n/a" || ingredients[i].units === "unit-type"
-          ? " "
-          : plural_adjusted + " of ";
-
-      const newText =
-        newQuantStr + " " + units_adjusted + ingredients[i].ingredient;
-
-      const newIngredient = {
-        ...ingredients[i],
-        quant: newQuant,
-        text: newText,
-      };
-
-      newIngredients.push(newIngredient);
-    }
-
-    setIngredients(newIngredients);
-  };
-
-  const handleDouble = () => {
-    const newIngredients = [];
-
-    for (let i = 0; i < ingredients.length; i++) {
-      const newQuantStr = (ingredients[i].quant * 2).toFixed(1);
-
-      console.log(newQuantStr);
-
-      const newQuant = parseFloat(newQuantStr);
-
-      console.log(newQuant);
-
-      const plural_adjusted =
-        newQuant === 1 ? ingredients[i].units : ingredients[i].units + "s";
-
-      const units_adjusted =
-        ingredients[i].units === "n/a" || ingredients[i].units === "unit-type"
+        ingredients[i].units === "n/a" || ingredients[i].unit === "unit-type"
           ? " "
           : plural_adjusted + " of ";
 
@@ -126,24 +94,22 @@ export default function IngredientsList() {
     <div className="recipe-app">
       <h1 className="my-recipe">My Recipe</h1>
       <div className="magic-buttons">
-        <form onSubmit={handleHalve}>
-          <button
-            type="submit"
-            className="halve-btn btn btn-outline-secondary btn-sm"
-          >
-            {" "}
-            Halve my Recipe!{" "}
-          </button>
-        </form>
-        <form onSubmit={handleDouble}>
-          <button
-            type="submit"
-            className="double-btn btn btn-outline-secondary btn-sm"
-          >
-            {" "}
-            Double my Recipe!{" "}
-          </button>
-        </form>
+        <button
+          type="submit"
+          className="halve-btn btn btn-outline-secondary btn-sm"
+          onClick={() => handleChange(0.5)}
+        >
+          {" "}
+          Halve my Recipe!{" "}
+        </button>
+        <button
+          type="submit"
+          className="double-btn btn btn-outline-secondary btn-sm"
+          onClick={() => handleChange(2)}
+        >
+          {" "}
+          Double my Recipe!{" "}
+        </button>
       </div>
       <div className="list">
         <AddIngredient addItem={addItem} />
